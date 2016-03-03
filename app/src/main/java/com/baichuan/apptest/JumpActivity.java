9c3b827dfc8b279959e681d9cf38d6d6559853cb
LiuBin <630459967@qq.com>
2016-03-03 16:30:39 +0800

    优化修正

--- app/src/main/java/com/baichuan/apptest/JumpActivity.java
+++ app/src/main/java/com/baichuan/apptest/JumpActivity.java
@@ -1,34 +1,18 @@
 package com.baichuan.apptest;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 /**
  * Created by Administrator on 2016/3/1.
  */
 public class JumpActivity extends Activity {
 
-	private Button btn1;
-	private String content = "你好";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_jump);
 
-		btn1 = (Button) findViewById(R.id.button_jump);
-		btn1.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent data = new Intent();
-				data.putExtra("data", content);
-				setResult(2,data);
-
-				finish();
-			}
-		});
 	}
 }
